486f329337c5d2476b13cb2312a507045d09fe3f
Erick Henrique <erick.tatui@hotmail.com>
2023-04-27 21:20:09 -0300

    Todos feitos (Bomba)

---
+++ fundamentos/secao-6-introducao-a-JS-e-testes-unitarios/dia-01-fluxo-de-excecoes/script.js
@@ -0,0 +1,144 @@
+const clients = [
+  {
+    name: 'João da Silva',
+    age: 28,
+    email: 'joao.silva@gmail.com',
+    phone: '(11) 5555-5555',
+    address: {
+      street: 'Rua dos Lírios',
+      number: 123,
+      neighborhood: 'Jardim Primavera',
+      city: 'São Paulo',
+      state: 'SP',
+      cep: '01234-567',
+    },
+  },
+  {
+    name: 'Maria Souza',
+    age: 35,
+    email: 'maria.souza@hotmail.com',
+    address: {
+      street: 'Rua dos Cravos',
+      number: 456,
+      neighborhood: 'Copacabana',
+      city: 'Rio de Janeiro',
+      state: 'RJ',
+      cep: '12345-678',
+    },
+  },
+  {
+    name: 'Pedro Oliveira',
+    age: 42,
+    email: 'pedro.oliveira@gmail.com',
+    address: {
+      street: 'Rua das Margaridas',
+      number: 789,
+      neighborhood: 'Boa Viagem',
+      city: 'Recife',
+      state: 'PE',
+      cep: '23456-789',
+    },
+  },
+  {
+    name: 'Ana Santos',
+    age: 25,
+    email: 'ana.santos@gmail.com',
+    phone: '(71) 5555-5555',
+    address: {
+      street: 'Rua dos Girassóis',
+      number: 1011,
+      neighborhood: 'Barra',
+      city: 'Salvador',
+      state: 'BA',
+      cep: '34567-890',
+    },
+  },
+  {
+    name: 'Luiz Costa',
+    age: 32,
+    email: 'luiz.costa@hotmail.com',
+    address: {
+      street: 'Rua das Acácias',
+      number: 1213,
+      neighborhood: 'Centro',
+      city: 'Belo Horizonte',
+      state: 'MG',
+      cep: '45678-901',
+    },
+  },
+  {
+    name: 'Isabela Almeida',
+    age: 37,
+    email: 'isabela.almeida@gmail.com',
+    phone: '(21) 5555-5555',
+    address: {
+      street: 'Rua das Hortênsias',
+      number: 1415,
+      neighborhood: 'Botafogo',
+      city: 'Rio de Janeiro',
+      state: 'RJ',
+      cep: '56789-012',
+    },
+  },
+  {
+    name: 'Rafael Ferreira',
+    age: 29,
+    email: 'rafael.ferreira@hotmail.com',
+    address: {
+      street: 'Rua das Orquídeas',
+      number: 1617,
+      neighborhood: 'Pinheiros',
+      city: 'São Paulo',
+      state: 'SP',
+      cep: '67890-123',
+    },
+  },
+];
+
+const findPersonByName = (name) => {
+    try {
+      for (i = 0; i < clients.length; i += 1) {
+        if (clients[i].name === name) {
+          return console.log(`Destinatário: ${clients[i].name}. Endereço: ${clients[i].address.street}, ${clients[i].address.number}, ${clients[i].address.neighborhood}, ${clients[i].address.state}, CEP: ${clients[i].address.cep}`); 
+        } 
+      }  
+      throw new Error("Pessoa não encontrada");
+    } catch(err) {
+      return console.log(err.message);
+    }
+};
+
+const findPersonByPosition = (position) => {
+  try {
+    if (position > clients.length || position < 0) {
+        throw new Error("Posição não encontrada");
+    } else if (isNaN(position)) {
+        throw new Error("Por favor digite um numero para achar a posição");
+    }
+    return console.log(`Cliente: ${clients[position].name}, email: ${clients[position].email}`);
+  } catch(err) {
+    return console.log(err.message);
+  }
+};
+
+const findPeopleByState = (state) => {
+  let pessoas = [];
+  let contador = 0;
+  try {
+    for (i = 0; i < clients.length; i += 1) {
+        if (clients[i].address.state === state) {
+            pessoas.push(clients[i].name)
+            contador += 1
+        }
+    }
+    if (contador > 0){
+      return console.log(pessoas)  
+    }
+    throw new Error("Ops, nenhuma pessoa mora nesse estado, tente outro")
+  } catch(err) {
+    return console.log(err.message)
+  }
+};
+findPersonByName('x');
+findPersonByPosition(12);
+findPeopleByState('PE')
